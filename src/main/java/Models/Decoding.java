2ced511299a92a592e87b242d38615f9c1d52168
Brian Kiarie Muigai <brian.muigai@student.moringaschool.com>
2021-12-07 06:59:43 +0300

    adding a for loop and a return

--- src/main/java/Models/Decoding.java
+++ src/main/java/Models/Decoding.java
@@ -9,5 +9,14 @@ public class Decoding {
         int index;
         String str="";
         char plain;
+
+        for (int i = 0; i < chr.length; i++){
+
+            index = chr[i] -97;
+            index = (index - key + 26) % 26;
+            plain = (char) (index + 97);
+            str = str + plain;
+        }
+        return str;
     }
 }
