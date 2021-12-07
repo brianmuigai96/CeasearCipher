9fb216d9b16f95c35b88a956eed4dc1bac578ac1
Brian Kiarie Muigai <brian.muigai@student.moringaschool.com>
2021-12-07 06:35:41 +0300

    creating a for loop and puting a return

--- src/main/java/Models/Encoding.java
+++ src/main/java/Models/Encoding.java
@@ -10,6 +10,15 @@ public class Encoding {
         String str="";
         char plain;
 
+        for (int i = 0; i < chr.length; i++){
+
+            index = chr[i] -97;
+            index = (index - key + 26) % 26;
+            plain = (char) (index + 97);
+            str = str + plain;
+        }
+        return str;
+    }
 
 
 }
