28c9836e354e9a56aed757826ad747a49f692940
Brian Kiarie Muigai <brian.muigai@student.moringaschool.com>
2021-12-07 06:34:56 +0300

    declaring a String,ind and char

--- src/main/java/Models/Encoding.java
+++ src/main/java/Models/Encoding.java
@@ -2,8 +2,14 @@ package Models;
 
 public class Encoding {
     public String decryption(String cipher,int shift){
+        String c = cipher.toLowerCase();
+        char chr [] = c.toCharArray();
+        int key = shift;
 
-    }
+        int index;
+        String str="";
+        char plain;
+
 
 
 }
